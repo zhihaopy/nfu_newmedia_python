834a8379a2b780843cf378d6ef22dba402ad36c8
kusumuxi <1350439869@qq.com>
2017-06-21 13:30:28 +0800

    Update pick_a_color_4web.py

--- pick_a_color_4web/pick_a_color_4web.py
+++ pick_a_color_4web/pick_a_color_4web.py
@@ -15,7 +15,7 @@ def pick_a_color() -> 'html':
     """提取用户web 请求POST方法提交的数据（输入），不执行任何动作（处理），直接返回（输出）。"""
     user_color = request.form['user_color']	
     return render_template('results.html',
-                           the_title = '以下是您选取的颜色：',
+                           the_title = '以下是你选取的颜色：',
                            the_color = user_color,
                            )
 
