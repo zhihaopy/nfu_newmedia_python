881b252f5161c1cf8cc3b6c9742586a9861fb360
Baizui <1906634789@qq.com>
2017-06-21 15:14:42 +0800

    做好了

--- province/province/province.py
+++ province/province/province.py
@@ -59,7 +59,7 @@ def do_search() -> 'html':
 def entry_page() -> 'html':
     """Display this webapp's HTML form."""
     return render_template('entry.html',
-                           the_title='欢迎来到网上省份与互联网普及率搜索!',
+                           the_title='欢迎来到网上省份互联网普及率搜索!',
     						   the_user_province=list_province)
     
 
