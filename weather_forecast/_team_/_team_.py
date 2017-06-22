325fa729f902a79be5d7ea1f56c2cc3d7bd595fc
Han-Teng Liao <hanteng@gmail.com>
2017-06-22 22:24:25 +0800

    先移除weather_forecast
    先移除weather_forecast

--- weather_forecast/_team_/_team_.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import csv
-with open('_team_.tsv', encoding='utf8') as f:
-    reader = csv.DictReader(f)
-    print ('\t'.join(reader.fieldnames))
-    for r in reader:
-        print ('\t'.join([r[k] for k in reader.fieldnames]) )
