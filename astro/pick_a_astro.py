53e58892d0163d26bdfb156c78caefc5df1da939
zhihaopy <784716740@qq.com>
2017-06-26 22:42:04 +0800

    二H 星座运势分析

--- astro/pick_a_astro.py
+++ astro/pick_a_astro.py
@@ -1,5 +1,10 @@
 from astro_notebooks import find_astro
 from flask import Flask, render_template, request, escape
+from glblood import get_blood_name
+
+c = get_blood_name()
+
+
 
 app = Flask(__name__)
 
@@ -17,13 +22,17 @@ def pick_a_astro() -> 'html':
     user_birthday_month = request.form["user_birthday_month"]	
     user_birthday_day = request.form["user_birthday_day"]
     user_astro = find_astro(user_birthday_month,user_birthday_day)
-	
+    user_blood_code = c[user_astro]
     return render_template('results.html',
                            the_title = '以下是您的星座：',
                            the_user_birthday_month = user_birthday_month,
                            the_user_birthday_day = user_birthday_day,
-                           the_user_astro = user_astro)
-                           
+                           the_user_astro = user_astro,
+                           the_blood_code = user_blood_code
+                           )
+
+    """提取用户web 请求POST方法提交的数据（输入），不执行任何动作（处理），直接返回（输出）。"""
+                          
 
 if __name__ == '__main__':
     app.run(debug=True)
