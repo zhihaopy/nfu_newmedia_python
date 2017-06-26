0136373157bcc95e858436b1d0032dc178de241c
zhihaopy <784716740@qq.com>
2017-06-26 13:08:24 +0800

    pick_a_astro.py
    最终成品。

---
+++ astro/astro_notebooks.py
@@ -0,0 +1,15 @@
+﻿import requests #用於API HTTP requests
+import json     #用於输入出json
+from pprint import pprint
+from datetime import datetime
+url_api = "http://api.jisuapi.com/astro/all"
+key={'appkey':'42e709a8d2927492'}
+r = requests.get (url_api, params=key)
+data = r.json()
+astro_dates_start={x['date'].split('~')[0]:x['astroname'] for x in data['result']}
+start_date_2_astro = {datetime.strptime(k, '%m-%d'):v for k,v in astro_dates_start.items()}
+start_dates = sorted(list(start_date_2_astro.keys()))
+
+def find_astro(month, day):
+    user_input = datetime.strptime('{m}-{d}'.format(m=month, d=day), '%m-%d')
+    return (start_date_2_astro[ start_dates[len([ user_input>=x for x in start_dates if (user_input>=x) == True])-1] ])
---
+++ astro/pick_a_astro.py
@@ -0,0 +1,29 @@
+from astro_notebooks import find_astro
+from flask import Flask, render_template, request, escape
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/entry')
+
+def entry_page() -> 'html':
+    """Display this webapp's HTML form."""
+    return render_template('entry.html',
+                           the_title='生日与星座！')
+
+@app.route('/pick_a_astro', methods=['POST'])
+def pick_a_astro() -> 'html':
+    """提取用户web 请求POST方法提交的数据（输入），不执行任何动作（处理），直接返回（输出）。"""
+    user_birthday_month = request.form["user_birthday_month"]	
+    user_birthday_day = request.form["user_birthday_day"]
+    user_astro = find_astro(user_birthday_month,user_birthday_day)
+	
+    return render_template('results.html',
+                           the_title = '以下是您的星座：',
+                           the_user_birthday_month = user_birthday_month,
+                           the_user_birthday_day = user_birthday_day,
+                           the_user_astro = user_astro)
+                           
+
+if __name__ == '__main__':
+    app.run(debug=True)
