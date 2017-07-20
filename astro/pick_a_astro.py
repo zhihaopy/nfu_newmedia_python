9b09e6c696cfbac1d53f65d3e1e6b72d5f2203ab
Bangajeong <13553955030@163.com>
2017-07-20 22:39:10 +0800

    Add files via upload

--- astro/pick_a_astro.py
+++ astro/pick_a_astro.py
@@ -1,4 +1,4 @@
-from astro_notebooks import find_astro
+﻿from astro_notebooks import find_astro
 from flask import Flask, render_template, request, escape
 from glblood import get_blood_name
 
@@ -19,9 +19,10 @@ def entry_page() -> 'html':
 @app.route('/pick_a_astro', methods=['POST'])
 def pick_a_astro() -> 'html':
     """提取用户web 请求POST方法提交的数据（输入），不执行任何动作（处理），直接返回（输出）。"""
-    user_birthday_month = request.form["user_birthday_month"]	
-    user_birthday_day = request.form["user_birthday_day"]
-    user_astro = find_astro(user_birthday_month,user_birthday_day)
+    user_birthdate = request.form["user_birthdate"]
+    user_birthday_month = user_birthdate.split("-")[1]
+    user_birthday_day = user_birthdate.split("-")[2]
+    user_astro = find_astro(user_birthday_month,user_birthday_day) 
     user_blood_code = c[user_astro]
     return render_template('results.html',
                            the_title = '以下是您的星座：',
